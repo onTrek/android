19fcb665380a56dc5156be4ab12142277e935966
Romitoskj <antormt@gmail.com>
2025-06-14 16:01:03 +0200

    Add Color.kt 🎨

---
+++ app/src/main/java/com/ontrecksmartwatch/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.ontrecksmartwatch.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkMossGreen = Color(0xff606c38)
+val PakistanGreen = Color(0xff283618)
+val Cornsilk = Color(0xFFFEFAE0)
+
+//val Purple40 = Color(0xFF6650a4)
+//val PurpleGrey40 = Color(0xFF625b71)
+//val Pink40 = Color(0xFF7D5260)
