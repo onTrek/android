7abc496da52841b6642bf8287a9f76f585c0d2e3
Romitoskj <antormt@gmail.com>
2025-06-17 12:37:02 +0200

    Remove default padding from NavigationStack

--- app/src/main/java/com/ontrecksmartwatch/MainActivity.kt
+++ app/src/main/java/com/ontrecksmartwatch/MainActivity.kt
@@ -6,9 +6,6 @@ import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ontrecksmartwatch.theme.OnTrekSmartwatchTheme
 
