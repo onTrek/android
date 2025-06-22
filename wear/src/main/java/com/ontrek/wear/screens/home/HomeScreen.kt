0cc8f1cbf79d897bdfcdeb68add2dc7b3b2e452e
Romitoskj <antormt@gmail.com>
2025-06-22 02:14:58 +0200

    Refactor HomeScreen to display empty states for track list

--- wear/src/main/java/com/ontrek/wear/screens/home/HomeScreen.kt
+++ wear/src/main/java/com/ontrek/wear/screens/home/HomeScreen.kt
@@ -1,8 +1,12 @@
 package com.ontrek.wear.screens.home
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -10,13 +14,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
 import androidx.wear.compose.foundation.lazy.ScalingLazyColumnDefaults
 import androidx.wear.compose.foundation.lazy.items
 import androidx.wear.compose.foundation.lazy.rememberScalingLazyListState
+import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.OutlinedButton
 import androidx.wear.compose.material.Text
@@ -24,6 +30,8 @@ import androidx.wear.compose.material3.ScreenScaffold
 import androidx.wear.compose.material3.ScrollIndicator
 import androidx.wear.compose.material3.ScrollIndicatorColors
 import androidx.wear.tooling.preview.devices.WearDevices
+import com.ontrek.shared.data.Track
+import com.ontrek.shared.data.TrackStats
 import com.ontrek.wear.screens.Screen
 import com.ontrek.wear.theme.OnTrekTheme
 
@@ -46,21 +54,54 @@ fun TrackSelectionScreen(navController: NavHostController, trackListState: LiveD
             )
         }
     ) {
+        // TODO: add a loading, error and empty states
 
-        ScalingLazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            state = listState,
-            flingBehavior = ScalingLazyColumnDefaults.snapFlingBehavior(state = listState),
-        ) {
-            item {
+        if (trackList.isNullOrEmpty()) {
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentHeight()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center
+            ) {
+                Icon(
+                    modifier = Modifier.padding(bottom = 8.dp),
+                    imageVector = Icons.Filled.Warning,
+                    contentDescription = "No tracks available",
+                    tint = MaterialTheme.colors.primary
+                )
                 Text(
                     modifier = Modifier.padding(bottom = 15.dp),
                     color = MaterialTheme.colors.primary,
-                    text = "My tracks"
+                    style = MaterialTheme.typography.title3,
+                    text = "No tracks available",
+                    textAlign = androidx.compose.ui.text.style.TextAlign.Center
+                )
+                Text(
+                    text = "Please upload tracks to view them here.",
+                    color = MaterialTheme.colors.secondary,
+                    style = MaterialTheme.typography.body2,
+                    textAlign = androidx.compose.ui.text.style.TextAlign.Center
                 )
             }
-            items(trackList ?: emptyList()) {
-                TrackButton(it.title, navController)
+        } else {
+            ScalingLazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                state = listState,
+                flingBehavior = ScalingLazyColumnDefaults.snapFlingBehavior(state = listState),
+            ) {
+                item {
+                    Text(
+                        modifier = Modifier.padding(bottom = 15.dp),
+                        color = MaterialTheme.colors.primary,
+                        style = MaterialTheme.typography.title2,
+                        text = "My tracks"
+                    )
+                }
+                items(trackList ?: emptyList()) {
+                    TrackButton(it.title, navController)
+                }
             }
         }
     }
@@ -75,7 +116,15 @@ fun TrackButton(trackName: String, navController: NavHostController) {
         modifier = Modifier
             .fillMaxWidth(0.95f)
     ) {
-        Text(trackName)
+        Text(
+            text = trackName,
+            style = MaterialTheme.typography.body1,
+            textAlign = androidx.compose.ui.text.style.TextAlign.Center,
+            modifier = Modifier
+                .padding(8.dp),
+            maxLines = 1,
+            overflow = androidx.compose.ui.text.style.TextOverflow.Ellipsis,
+        )
     }
 }
 
