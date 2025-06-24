322a5b8ed0cdd71b2b7a379bdad59be9749bfb57
Romitoskj <antormt@gmail.com>
2025-06-24 13:59:27 +0200

    Refactor Login screen layout and improve token handling logic

--- wear/src/main/java/com/ontrek/wear/screens/login/Login.kt
+++ wear/src/main/java/com/ontrek/wear/screens/login/Login.kt
@@ -1,92 +1,60 @@
 package com.ontrek.wear.screens.login
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Login
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.OutlinedButton
 import androidx.wear.compose.material.Text
-import com.ontrek.wear.R
+import androidx.wear.compose.material3.ScreenScaffold
 import com.ontrek.wear.data.PreferencesViewModel
 
 @Composable
-fun Login(modifier: Modifier = Modifier, preferencesViewModel : PreferencesViewModel = viewModel(factory = PreferencesViewModel.Factory)) {
-    Box(
-        modifier
+fun Login(
+    preferencesViewModel: PreferencesViewModel = viewModel(factory = PreferencesViewModel.Factory)
+) {
+    ScreenScaffold(
     ) {
-        Box(
-            modifier = modifier.fillMaxSize()
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Column(
+            Icon(
+                imageVector = Icons.AutoMirrored.Outlined.Login,
+                contentDescription = "Warning Icon",
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Text(
+                text = "In order to login, please open the OnTrek app on your phone.",
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+            //Only for debug purposes
+            OutlinedButton(
+                onClick = {
+                    preferencesViewModel.saveToken("3e530eef-2f77-418e-89e7-d82537c9109a") //fuck it we ball
+                },
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                    .fillMaxWidth(0.95f)
+                    .padding(5.dp),
             ) {
-
-                Text(
-                    text = "In order to login, please open the OnTrek app on your phone.",
-                    fontSize = 16.sp,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.padding(bottom = 4.dp)
-                )
-                //Only for debug purposes
-                OutlinedButton(
-                    onClick = {
-                        preferencesViewModel.saveToken("3e530eef-2f77-418e-89e7-d82537c9109a") //fuck it we ball
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth(0.95f)
-                ) {
-                    Text("Skip Login")
-                }
+                Text("Skip Login")
             }
         }
-    }
-}
-
-@Composable
-fun AppLogo(
-    modifier: Modifier = Modifier,
-    appName: String = "OnTrek",           // Optional app name
-) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Image(
-                //da aggiungere poi il nostro logo
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                contentDescription = "App Logo",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
-            )
-
-        }
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Text(
-            text = appName,
-            fontSize = 20.sp,
-        )
     }
 }
