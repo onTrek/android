232c0cdf024ba495041a5984f4b61169e5ac7363
Romitoskj <antormt@gmail.com>
2025-06-25 17:32:45 +0200

    Refactor SOSScreen for Material3 theming and improved button accessibility

--- wear/src/main/java/com/ontrek/wear/screens/sos/SOSScreen.kt
+++ wear/src/main/java/com/ontrek/wear/screens/sos/SOSScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,13 +24,16 @@ import androidx.navigation.compose.rememberNavController
 import androidx.wear.compose.material3.AlertDialog
 import androidx.wear.compose.material3.Button
 import androidx.wear.compose.material3.ButtonDefaults
+import androidx.wear.compose.material3.FilledTonalIconButton
 import androidx.wear.compose.material3.Icon
 import androidx.wear.compose.material3.IconButton
+import androidx.wear.compose.material3.IconButtonColors
 import androidx.wear.compose.material3.MaterialTheme
 import androidx.wear.compose.material3.ScreenScaffold
 import androidx.wear.compose.material3.Text
 import androidx.wear.compose.material3.TimeText
 import androidx.wear.compose.material3.curvedText
+import androidx.wear.compose.material3.touchTargetAwareSize
 import androidx.wear.tooling.preview.devices.WearDevices
 import com.ontrek.wear.theme.OnTrekTheme
 
@@ -37,13 +41,13 @@ import com.ontrek.wear.theme.OnTrekTheme
 @Composable
 fun SOSScreen(navController: NavHostController) {
     var showDialog by remember { mutableStateOf(false) }
-    val textColor = MaterialTheme.colorScheme.onErrorContainer.copy(alpha = 0.8f)
+    val textColor = MaterialTheme.colorScheme.onErrorContainer
 
     ScreenScaffold(
         modifier = Modifier.background(MaterialTheme.colorScheme.errorContainer),
         timeText = {
             TimeText(
-                backgroundColor = MaterialTheme.colorScheme.onError
+                backgroundColor = MaterialTheme.colorScheme.onError.copy(alpha = 0.8f)
             ) { time ->
                 curvedText(
                     text = "Help coming!",  // TODO: SOS send (if no one responded yet)
@@ -57,19 +61,24 @@ fun SOSScreen(navController: NavHostController) {
         Box(
             modifier = Modifier
                 .fillMaxSize(),
-            contentAlignment = androidx.compose.ui.Alignment.Center
+            contentAlignment = Alignment.Center
         ) {
             Icon(
                 imageVector = Icons.Filled.MyLocation,
                 contentDescription = "Location",
                 tint = MaterialTheme.colorScheme.onErrorContainer
             )
-            IconButton(
+            FilledTonalIconButton (
                 onClick = { showDialog = true },
-
+                colors = IconButtonColors(
+                    containerColor = MaterialTheme.colorScheme.onError,
+                    contentColor = MaterialTheme.colorScheme.onErrorContainer.copy(alpha = 0.8f),
+                    disabledContainerColor = MaterialTheme.colorScheme.errorContainer,
+                    disabledContentColor = MaterialTheme.colorScheme.onErrorContainer.copy(alpha = 0.8f),
+                ),
                 modifier = Modifier
-                    .align(androidx.compose.ui.Alignment.BottomCenter)
-                    .fillMaxSize(0.15f),
+                    .align(Alignment.BottomCenter)
+                    .touchTargetAwareSize(25.dp),
             ) {
                 Icon(
                     imageVector = Icons.Filled.Close,
