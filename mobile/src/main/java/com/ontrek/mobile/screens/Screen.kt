a66b3405943ec0cd03109983ee7fb390e3e79062
Romitoskj <antormt@gmail.com>
2025-07-01 16:05:08 +0200

    Remove AuthScreen from Screen sealed class

--- mobile/src/main/java/com/ontrek/mobile/screens/Screen.kt
+++ mobile/src/main/java/com/ontrek/mobile/screens/Screen.kt
@@ -1,7 +1,6 @@
 package com.ontrek.mobile.screens
 
 sealed class Screen(val route: String) {
-    object AuthScreen : Screen("AuthScreen")
     object ConnectionScreen : Screen("ConnectionScreen")
     object FriendsScreen : Screen("FriendsScreen")
     object TracksScreen : Screen("TracksScreen")
