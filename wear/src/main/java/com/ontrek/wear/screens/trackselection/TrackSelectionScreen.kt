42415d54c538f0281ee6e55830c66ba2c927e5c5
Antonio Pietro Romito <72152346+Romitoskj@users.noreply.github.com>
2025-07-02 16:22:37 +0200

    Merge pull request #29
    * Refactor TrackSelectionScreen to optimize token handling and reduce u…
    
    * Remove unused imports and streamline code
    
    * Refactor TrackSelectionScreen and ViewModel to enhance loading state …
    
    * Refactor TrackSelectionScreen to optimize token handling and reduce u…
    
    * Remove unused imports and streamline code
    
    * Refactor TrackSelectionScreen and ViewModel to enhance loading state …
    
    * Merge remote-tracking branch 'origin/19-displaying-fetched-tracks' in…

--- wear/src/main/java/com/ontrek/wear/screens/trackselection/TrackSelectionScreen.kt
+++ wear/src/main/java/com/ontrek/wear/screens/trackselection/TrackSelectionScreen.kt
@@ -3,6 +3,7 @@ package com.ontrek.wear.screens.trackselection
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -81,12 +82,8 @@ fun TrackSelectionScreen(
     ) {
 
 
-        if (token.isNullOrEmpty() || isLoading) {
+        if (isLoading) {
             Loading(modifier = Modifier.fillMaxSize())
-            Log.d(
-                "TrackSelectionScreen",
-                "Loading tracks with token: $token, isLoading: $isLoading"
-            )
         } else if (!error.isNullOrEmpty()) {
             ErrorFetch(token, fetchTrackList)
         } else if (trackList.isEmpty()) {
