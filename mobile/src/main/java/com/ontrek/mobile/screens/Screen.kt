cbc4528edd1460bb55026c57f1dceb637ab5a402
Romitoskj <antormt@gmail.com>
2025-07-19 12:58:29 +0200

    refactor: implement nested navigation structure for bottom navigation and screens

--- mobile/src/main/java/com/ontrek/mobile/screens/Screen.kt
+++ mobile/src/main/java/com/ontrek/mobile/screens/Screen.kt
@@ -1,12 +1,21 @@
 package com.ontrek.mobile.screens
 
 sealed class Screen(val route: String) {
-        object Profile : Screen("ProfileScreen")
-        object Friends : Screen("FriendsScreen")
-        object Tracks : Screen("TracksScreen")
-        object Hikes : Screen("HikesScreen")
-        object TrackDetail : Screen("TrackDetailScreen/{trackId}") {
-            fun createRoute(trackId: String) = "TrackDetailScreen/$trackId"
-        }
+    object Profile : Screen("ProfileScreen")
+    object Friends : Screen("FriendsScreen")
+    object Tracks : Screen("TracksScreen")
+    object Hikes : Screen("HikesScreen")
+    object TrackDetail : Screen("TrackDetailScreen/{trackId}") {
+        fun createRoute(trackId: String) = "TrackDetailScreen/$trackId"
     }
+}
+
+
+sealed class TopLevelScreen(route: String, val title: String) : Screen(route) {
+    object Profile : TopLevelScreen("ProfileSection", "Profile")
+    object Friends : TopLevelScreen("FriendsSection", "Friends")
+    object Tracks : TopLevelScreen("TracksSection", "Tracks")
+    object Hikes : TopLevelScreen("HikesSection", "Hikes")
+
+}
 
