a4be944af49561b83d4eb5df56a6cfe92660ac1b
Romitoskj <antormt@gmail.com>
2025-07-20 14:08:07 +0200

    Remove unused error icon import from TrackSelectionScreen

--- wear/src/main/java/com/ontrek/wear/screens/trackselection/TrackSelectionScreen.kt
+++ wear/src/main/java/com/ontrek/wear/screens/trackselection/TrackSelectionScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.CloudOff
-import androidx.compose.material.icons.outlined.Error
 import androidx.compose.material.icons.outlined.Refresh
 import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.runtime.Composable
